50d29b792eb6b074e795c348f823455d179a70dd
Igor Crnković <17ender@gmail.com>
2014-04-29 00:45:29 +0200

    dev enviroment setup

---
+++ django_project/core/settings/dev_icrni.py
@@ -0,0 +1,32 @@
+from .dev import *
+
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # Or path to database file if using sqlite3.
+        'NAME': 'nyaruka',
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        # Empty for localhost through domain sockets or '127.0.0.1' for
+        # localhost through TCP.
+        'HOST': '',
+        # Set to empty string for default.
+        'PORT': '',
+        'CONN_MAX_AGE': None
+    }
+}
+
+
+# django rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        # enable browsable API for development
+        'rest_framework.renderers.BrowsableAPIRenderer'
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
