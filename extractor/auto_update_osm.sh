585753da3ab534b13a805b0115256095494e99ab
Dražen Odobašić <dodobas@candela-it.com>
2014-09-01 14:54:28 +0200

    Update bash auto_update utilities

--- extractor/auto_update_osm.sh
+++ extractor/auto_update_osm.sh
@@ -3,16 +3,16 @@ CWD=$(pwd)
 
 
 cd $WORKING_DIRECTORY
-mv $PLANET_OSM $PLANET_OSM.last
+mv $PLANET_OSM.o5m $PLANET_OSM.o5m.last
 
 echo "Updating planet.osm with the latest changes..."
-./osmupdate -v $PLANET_OSM.last $PLANET_OSM
+./osmupdate -v $PLANET_OSM.o5m.last $PLANET_OSM.o5m
 
 echo "Creating admin_level backup file..."
 mv admin_levels.o5m admin_levels.o5m.last
 
 echo "Extracting admin_levels..."
-./osmfilter $PLANET_OSM --keep=admin_level -o=admin_levels.o5m
+./osmfilter $PLANET_OSM.o5m --keep=admin_level -o=admin_levels.o5m
 
 echo "Writing admin_level.pbf file... $(pwd)/admin_levels.pbf"
 ./osmconvert admin_levels.o5m -o=admin_levels.pbf
