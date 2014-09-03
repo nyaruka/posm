66ac8fb603e39dc21c0a704f50e1bb89286da0e8
Dražen Odobašić <dodobas@candela-it.com>
2014-09-03 17:10:04 +0200

    Fix process error messages handling

--- extractor/POSMmanagement/utils.py
+++ extractor/POSMmanagement/utils.py
@@ -3,13 +3,13 @@ from cStringIO import StringIO
 
 def proc_exec(proc, verbose):
     # execute the process ... .wait()
-    if verbose:
-        proc_output = StringIO()
-        while proc.poll() is None:
-            line = proc.stdout.readline()
-            print line,
-            proc_output.write(line)
-        msg = ''.join(proc_output)
-    else:
-        msg = ''.join(proc.communicate())
+    proc_output = StringIO()
+
+    while proc.poll() is None:
+        line = proc.stdout.read()
+        if verbose:
+            print line
+        proc_output.write(line)
+    msg = ''.join(proc_output.getvalue())
+
     return msg
