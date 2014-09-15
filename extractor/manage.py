30221e4dcdd37a3ab2ce6295022a663e1ee8010a
Dražen Odobašić <dodobas@candela-it.com>
2014-09-15 15:30:42 +0200

    Separate update and extract_simplify commands

--- extractor/manage.py
+++ extractor/manage.py
@@ -13,7 +13,7 @@ from POSMmanagement.process import ProcessManagement
 from POSMmanagement.project import ProjectManagement
 
 
-def run_all(args):
+def update_data(args):
     # initialize settings
     proj_settings = POSMSettings(args.settings, verbose=args.verbose)
 
@@ -21,12 +21,23 @@ def run_all(args):
     osm_man.updateOSM()
     osm_man.extractAdminLevels()
     osm_man.convertO5MtoPBF()
+
+
+def extract_and_simplify(args):
+    # initialize settings
+    proj_settings = POSMSettings(args.settings, verbose=args.verbose)
+
     ext_sim = ProcessManagement(proj_settings, verbose=args.verbose)
     ext_sim.processAdminLevels(args.settings)
     ext_sim.deconstructGeometry()
     ext_sim.createBaseTopology()
     ext_sim.simplifyAdminLevels(args.tolerance)
     ext_sim.convertToGeoJson(args.settings)
+
+
+def run_all(args):
+    update_data(args)
+    extract_and_simplify(args)
 
 
 def download_OSM(args):
@@ -58,6 +69,7 @@ def cut_data(args):
 parser = argparse.ArgumentParser(description='Manage common POSM tasks')
 subparsers = parser.add_subparsers(title='Tasks')
 
+# run_all
 parser_run_all = subparsers.add_parser(
     'run_all', help='updateOSM data, extractAdminLevels, simplifyAdminLevels'
 )
@@ -70,7 +82,27 @@ parser_run_all.add_argument(
 )
 parser_run_all.set_defaults(func=run_all)
 
+# update_data
+parser_update_data = subparsers.add_parser(
+    'update_data', help='updateOSM data'
+)
 
+parser_update_data.set_defaults(func=update_data)
+
+# extract_and_simplify
+parser_ext_sim = subparsers.add_parser(
+    'extract_and_simplify', help='extractAdminLevels, simplifyAdminLevels'
+)
+parser_ext_sim.add_argument(
+    '--tolerance', type=float, default=0.001,
+    help=(
+        'Tolerance parameter for DouglasPeucker simplification algorithm '
+        '(default: 0.001)'
+    )
+)
+parser_ext_sim.set_defaults(func=extract_and_simplify)
+
+# download_OSM
 parser_download_OSM = subparsers.add_parser(
     'download_OSM',
     help='downloads OSM data'
@@ -79,21 +111,21 @@ parser_download_OSM.add_argument('data_url', help='OSM data http URI')
 
 parser_download_OSM.set_defaults(func=download_OSM)
 
-
+# create_DB
 parser_create_DB = subparsers.add_parser(
     'create_DB',
     help='creates new PostGIS database and load functions'
 )
 parser_create_DB.set_defaults(func=create_DB)
 
-
+# init_dir
 parser_init_dir = subparsers.add_parser(
     'init_dir', help='initializes empty data directory'
 )
 
 parser_init_dir.set_defaults(func=init_dir)
 
-
+# cut_data
 parser_cut_data = subparsers.add_parser(
     'cut_data', help='cuts OSM data from the planet osm file'
 )
@@ -103,6 +135,7 @@ parser_cut_data.add_argument(
 parser_cut_data.set_defaults(func=cut_data)
 
 
+# script arguments
 parser.add_argument(
     '--verbose', action='store_true', default=False,
     help='show verbose execution messages'
